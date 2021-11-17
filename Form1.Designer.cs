b8b5a2b9b78c44c5a08c5e63e367f2dc4ff9079b
ashleyyuloo <ashleyyulo04@yahoo.com>
2021-11-17 21:27:59 +0800

    delete label

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,7 +32,6 @@ namespace FOTIS1B_Cabanero_Sumalabe_FinalMP
             this.startgamebutton = new System.Windows.Forms.Button();
             this.mechanicsbutton = new System.Windows.Forms.Button();
             this.exitgamemainbutton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // startgamebutton
@@ -65,28 +64,17 @@ namespace FOTIS1B_Cabanero_Sumalabe_FinalMP
             this.exitgamemainbutton.UseVisualStyleBackColor = true;
             this.exitgamemainbutton.Click += new System.EventHandler(this.exitgamemainbutton_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(40, 144);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "hi gio!~";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(378, 456);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.exitgamemainbutton);
             this.Controls.Add(this.mechanicsbutton);
             this.Controls.Add(this.startgamebutton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -95,7 +83,6 @@ namespace FOTIS1B_Cabanero_Sumalabe_FinalMP
         private System.Windows.Forms.Button startgamebutton;
         private System.Windows.Forms.Button mechanicsbutton;
         private System.Windows.Forms.Button exitgamemainbutton;
-        private System.Windows.Forms.Label label1;
     }
 }
 
